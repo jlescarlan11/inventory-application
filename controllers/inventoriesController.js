010d94068fa7ed2d1a841e22c80a21dc65ab54d7
jlescarlan11 <jlescarlan11@gmail.com>
2025-04-08 09:59:26 +0800

    added a navbar

--- controllers/inventoriesController.js
+++ controllers/inventoriesController.js
@@ -1,3 +1,9 @@
+navItems = [
+  { name: "Home", href: "/" },
+  { name: "Categories", href: "/categories" },
+  { name: "Items", href: "/items" },
+];
+
 exports.inventoriesListGet = async (req, res, next) => {
-  res.render("index");
+  res.render("index", { navItems });
 };
